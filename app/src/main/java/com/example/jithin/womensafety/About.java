39a838d74c84689b3a812cbd1bf372bad96db394
Shalini  V <105047114+shaal123@users.noreply.github.com>
2023-10-27 23:51:33 +0530

    app

---
+++ app/src/main/java/com/example/jithin/womensafety/About.java
@@ -0,0 +1,19 @@
+package com.example.jithin.womensafety;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import androidx.appcompat.widget.Toolbar;
+
+public class About extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+        setTitle("About");
+
+
+
+    }
+}
